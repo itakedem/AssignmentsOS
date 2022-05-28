084d24b7df44a97c8a6fe524d5fe50b906f0c3e5
taliBerman5 <taliberman5@gmail.com>
2022-05-28 17:50:46 +0300

    added changes

--- Assignment3/xv6-riscv-riscv/kernel/defs.h
+++ Assignment3/xv6-riscv-riscv/kernel/defs.h
@@ -63,6 +63,9 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+void            inc_reference_count(uint64 pa);
+void            dec_reference_count(uint64 pa);
+int             get_reference_count(uint64 pa);
 
 // log.c
 void            initlog(int, struct superblock*);
@@ -145,6 +148,7 @@ void            trapinit(void);
 void            trapinithart(void);
 extern struct spinlock tickslock;
 void            usertrapret(void);
+int             cow_handler(pagetable_t pagetable, uint64 va);
 
 // uart.c
 void            uartinit(void);
@@ -170,6 +174,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+
 
 // plic.c
 void            plicinit(void);
--- Assignment3/xv6-riscv-riscv/kernel/kalloc.c
+++ Assignment3/xv6-riscv-riscv/kernel/kalloc.c
@@ -9,34 +9,39 @@
 #include "riscv.h"
 #include "defs.h"
 
+#define NUM_PYS_PAGES ((PHYSTOP-KERNBASE) / PGSIZE)
 void freerange(void *pa_start, void *pa_end);
-
+extern uint64 cas(volatile void *addr, int expected, int newval);
 extern char end[]; // first address after kernel.
-                   // defined by kernel.ld.
+// defined by kernel.ld.
 
 struct run {
-  struct run *next;
+    struct run *next;
 };
 
 struct {
-  struct spinlock lock;
-  struct run *freelist;
+    struct spinlock lock;
+    struct run *freelist;
+    uint page_ref_count[NUM_PYS_PAGES];
 } kmem;
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+    initlock(&kmem.lock, "kmem");
+    memset(kmem.page_ref_count, 0, sizeof(kmem.page_ref_count));
+    freerange(end, (void*)PHYSTOP);
 }
 
 void
 freerange(void *pa_start, void *pa_end)
 {
-  char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+    char *p;
+    p = (char*)PGROUNDUP((uint64)pa_start);
+    for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+        kmem.page_ref_count[(((uint64)p-KERNBASE) / PGSIZE)] = 1;// TODO
+        kfree(p);
+    }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -46,20 +51,24 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
-  struct run *r;
+    struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfree");
+    if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+        panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+    dec_reference_count((uint64)pa);
+    if (get_reference_count((uint64)pa) > 0)
+        return;
 
-  r = (struct run*)pa;
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, PGSIZE);
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+    r = (struct run*)pa;
+
+    acquire(&kmem.lock);
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+    release(&kmem.lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,15 +77,34 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
-  struct run *r;
+    struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    acquire(&kmem.lock);
+    r = kmem.freelist;
+    if(r)
+        kmem.freelist = r->next;
+    release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+    if(r)
+        memset((char*)r, 5, PGSIZE); // fill with junk
+
+    if(r)
+        inc_reference_count((uint64)r);
+
+    return (void*)r;
+}
+
+
+void inc_reference_count(uint64 pa){
+    uint64 entry = (pa-KERNBASE) / PGSIZE;
+    while(cas(kmem.page_ref_count + entry, kmem.page_ref_count[entry], kmem.page_ref_count[entry] + 1));
+}
+
+void dec_reference_count(uint64 pa){
+    uint64 entry = (pa-KERNBASE) / PGSIZE;
+    while(cas(kmem.page_ref_count + entry, kmem.page_ref_count[entry], kmem.page_ref_count[entry] - 1));
+}
+
+int get_reference_count(uint64 pa){
+    return kmem.page_ref_count[(pa-KERNBASE) / PGSIZE];
 }
